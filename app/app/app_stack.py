43ed8ba936b196034a50c937104b088dae7405df
Reef Proctor <Tranzitdecoder@gmail.com>
2022-09-23 22:00:19 +1200

    quick fix for function

--- app/app/app_stack.py
+++ app/app/app_stack.py
@@ -16,13 +16,15 @@ class AppStack(Stack):
         ##################### stack is defined within here###################################################
         self.TempFileName()
         self.CreateUserPool()
-    
-    def TempFileName(self)
+
+    def TempFileName(self):
         # create a python for packages (installed at app/app/layers)
         layer = lambda_.LayerVersion(self, 'astro_layer',
-                                     code=lambda_.Code.from_asset("app/layers"),
+                                     code=lambda_.Code.from_asset(
+                                         "app/layers"),
                                      description='packages for mast lambda',
-                                     compatible_runtimes=[lambda_.Runtime.PYTHON_3_9],
+                                     compatible_runtimes=[
+                                         lambda_.Runtime.PYTHON_3_9],
                                      removal_policy=cdk.RemovalPolicy.DESTROY
                                      )
         # lambda function to access s3 bucket where data is stored
@@ -32,7 +34,7 @@ class AppStack(Stack):
                               handler="get_data_s3_hubble.get_data",
                               layers=[layer],
                               )
-        #bucket to store uploaded photos (lambda triggers on upload to this bucket)
+        # bucket to store uploaded photos (lambda triggers on upload to this bucket)
         bucket_upload = s3.Bucket(self, "output-bucket",
                                   bucket_name="upload-picture-here",
                                   removal_policy=cdk.RemovalPolicy.DESTROY,
@@ -41,15 +43,17 @@ class AppStack(Stack):
                                   )
         # add an event to the bucket that triggers an event when anything is uploaded to it
         bucket_upload.add_event_notification(s3.EventType.OBJECT_CREATED,
-                                             s3_notifications.LambdaDestination(fn)
+                                             s3_notifications.LambdaDestination(
+                                                 fn)
                                              )
-        #bucket where the results are stored/returned
+        # bucket where the results are stored/returned
         bucket_results = s3.Bucket(self, "MAST-results-bucket575",
                                    bucket_name="mast-results-bucket575",
                                    removal_policy=cdk.RemovalPolicy.DESTROY,
                                    auto_delete_objects=True,
                                    block_public_access=s3.BlockPublicAccess.BLOCK_ALL
                                    )
+
     def CreateUserPool(self):
         # specify the configuration of the user pool
         cognito_userPool = cognito.UserPool(
